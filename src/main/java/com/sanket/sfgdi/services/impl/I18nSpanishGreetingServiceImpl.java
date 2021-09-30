c759e52d6b4c2a973ab4ba5ab905c6de73063f5d
sangupte <sangupte@cisco.com>
2021-09-30 21:47:06 +0530

    Dependency Injection using Spring - Default Profiles

--- src/main/java/com/sanket/sfgdi/services/impl/I18nSpanishGreetingServiceImpl.java
+++ src/main/java/com/sanket/sfgdi/services/impl/I18nSpanishGreetingServiceImpl.java
@@ -4,7 +4,7 @@ import com.sanket.sfgdi.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingServiceImpl implements GreetingService {
     @Override
